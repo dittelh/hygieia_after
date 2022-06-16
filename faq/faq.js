b2572ac02b92ce6a6bc3cedca740ff268f8f6d21
Ditte Lykke Hansen <dittehansen143@gmail.com>
2022-06-16 22:24:36 +0200

    js faq

--- faq/faq.js
+++ faq/faq.js
@@ -2,11 +2,11 @@
 function openAnswer(id){
 
     var answer = document.getElementById("answer" + id);
-    answer.classList.toggle("hideAnswer");
-    answer.classList.toggle("answerSection");
+    answer.classList.toggle("showAnswer");
+    
 
     var img = document.getElementById("img" + id);
-    if(answer.classList.contains("answerSection")){
+    if(answer.classList.contains("showAnswer")){
         img.src = "../assets/img/minus.svg";
     }else {
         img.src = "../assets/img/plus.svg";
